5f1130a8aa83f2d95fa58a3076038e05d89ce219
nicolasna77 <nicolas_abreu@live.fr>
2025-04-25 19:35:15 +0200

    fixe error prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker/locale/fr";
 
 const prisma = new PrismaClient();
