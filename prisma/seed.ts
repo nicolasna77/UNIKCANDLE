13abe0a0f37dc9057856fd60437f703e7c221fac
nicolasna77 <nicolas_abreu@live.fr>
2025-04-25 19:30:53 +0200

    fixe error prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,7 @@
-import prisma from "@/lib/prisma";
+import { PrismaClient } from "../generated/prisma";
 import { faker } from "@faker-js/faker/locale/fr";
+
+const prisma = new PrismaClient();
 
 async function main() {
   // Nettoyage de la base de données
