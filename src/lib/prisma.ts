eb8a93983ed55b03e35c23f0bc0e69a8026498af
nicolasna77 <nicolas_abreu@live.fr>
2025-09-15 00:28:50 +0200

    Mise à jour de l'importation de PrismaClient pour utiliser la version générée et amélioration de la gestion de l'instance Prisma.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,12 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@/generated/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma };
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
