181841a111a1633b2277b89ed02af6724f09ed4e
nicolasna77 <nicolas_abreu@live.fr>
2025-12-06 12:48:08 +0100

    feat: ajuster la hauteur maximale du contenu du dialogue dans le formulaire d'édition de produit

--- src/app/[locale]/(private)/admin/products/EditProductForm.tsx
+++ src/app/[locale]/(private)/admin/products/EditProductForm.tsx
@@ -253,7 +253,7 @@ export default function EditProductForm({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-5xl overflow-y-auto ">
+      <DialogContent className="max-w-5xl max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2">
             <Package className="h-5 w-5" />
